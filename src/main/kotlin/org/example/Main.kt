7e7ac14c35cfb1f3b5535521d9a81c0329d89d63
Paul Adams <ghusiyy@gmail.com>
2022-04-03 22:39:14 -0700

    create endpoint to receive current Hijri date

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,9 +1,12 @@
 package org.example
-
+import java.time.chrono.*
 import io.javalin.Javalin
+
+
 
 fun main() {
     val app = Javalin.create().start(7070)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    app.get("/dates/today") {ctx -> ctx.result(HijrahDate.now().toString())}
+    app.get("/") { ctx -> ctx.result("project thabit api") }
 }
 
