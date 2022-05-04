c7bcf1e0309bab6bacd1614168978d1e35f9979c
Paul Adams <ghusiyy@gmail.com>
2022-05-03 23:20:48 -0700

    make use of ApiBuilder to build routes

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,12 +1,31 @@
 package org.example
 import java.time.chrono.*
 import io.javalin.Javalin
-
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
 
 
 fun main() {
     val app = Javalin.create().start(7070)
-    app.get("/dates/today") {ctx -> ctx.result(HijrahDate.now().toString())}
-    app.get("/") { ctx -> ctx.result("project thabit api") }
+    app.routes {
+        path("/") {
+            get { ctx -> ctx.result("project thabit api") }
+        }
+        path("dates") {
+            path("now") {
+                get {
+                    // Obtains the current HijrahDate of the Islamic Umm Al-Qura calendar
+                    // in the default time-zone.
+                    ctx -> ctx.result(HijrahDate.now().toString())
+                }
+            }
+        }
+        path("measurements") {
+            get { ctx -> ctx.result("measurements") }
+        }
+        path("weights") {
+            get { ctx -> ctx.result("weights") }
+        }
+    }
 }
 
