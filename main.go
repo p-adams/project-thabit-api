4ae3e175a5f416c6be221dda4e3513a295cd7e55
Paul Adams <ghusiyy@gmail.com>
2023-05-19 00:25:24 -0700

    add sample endpoints

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "test")
+}
+
+func meowHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "meow")
+}
+
 func main() {
-	println("Hello, World")
+	
+	http.HandleFunc("/", rootHandler)
+    http.HandleFunc("/meow", meowHandler)
+
+    fmt.Println("Server listening on port 8080...")
+    http.ListenAndServe(":8080", nil)
 }
