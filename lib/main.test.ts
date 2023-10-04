8c4687a35a8ee70ce75748880740c01b05631348
Paul Adams <ghusiyy@gmail.com>
2023-10-03 23:17:49 -0700

    add tests

--- lib/main.test.ts
+++ lib/main.test.ts
@@ -1,9 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
+import { convertIslamicDistance, convertIslamicWeight } from "./main.ts";
 
 Deno.test("Distance", () => {
-  assertEquals(2, 2);
+  assertEquals(7.46, convertIslamicDistance("Farsakh (فرسخ)", 2, "us"));
 });
 
 Deno.test("Weight", () => {
-  assertEquals(2, 2);
+  assertEquals(14.875, convertIslamicWeight("Mithqal (مثقال)", 3.5, "metric"));
 });
