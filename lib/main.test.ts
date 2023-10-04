94fc891710384d592e8dba2dc7a4ff90a43e4524
Paul Adams <ghusiyy@gmail.com>
2023-10-03 22:32:07 -0700

    add test file

---
+++ lib/main.test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
+
+Deno.test("Distance", () => {
+  assertEquals(2, 2);
+});
+
+Deno.test("Weight", () => {
+  assertEquals(2, 2);
+});
